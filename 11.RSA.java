6212d88837e0cf9db352f47d353b66d22110fb23
Shivam Nagpal <shivamnagpal432@gmail.com>
2018-08-27 17:52:35 +0530

    updated

--- 11.RSA.java
+++ 11.RSA.java
@@ -1,110 +1,65 @@
-
-/*
-11. Write a program for simple RSA algorithm to encrypt and decrypt the data.
-*/
-
+import java.math.BigInteger;
+import java.security.SecureRandom;
 import java.util.Scanner;
 
 class RSA {
-    static int p, q;
-    static long n, phi_n;
 
-    static long gcd(long m, long n) {
-        long c = m % n;
-        if (c == 0) {
-            return n;
-        }
-        return gcd(n, c);
+    static BigInteger p, q, n, phi_n, e, d;
+    static SecureRandom secureRandom;
+    static int bitLength = 50;
+
+
+    static String encrypt(String msg) {
+        return new BigInteger(msg).modPow(e, n).toString();
     }
 
-    static long calculate_e() {
-        for (long i = 2; i < Long.MAX_VALUE; i++) {
-            if (gcd(i, phi_n) == 1) {
-                return i;
-            }
-        }
-        return 0;
-    }
-
-    static long calculate_d(long e) {
-        for (long i = 1; i < Long.MAX_VALUE; i++) {
-            if ((e * i) % phi_n == 1) {
-                return i;
-            }
-        }
-        return 0;
-    }
-
-    static long encrypt(long m, long e) {
-        return (long) (Math.pow(m, e) % n);
-    }
-
-    static long decrypt(long c, long d) {
-        return (long) (Math.pow(c, d) % n);
+    static String decrypt(String cipher) {
+        return new BigInteger(cipher).modPow(d, n).toString();
     }
 
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter two prime numbers");
-        p = scanner.nextInt();
-        q = scanner.nextInt();
+        secureRandom = new SecureRandom();
 
-        n = p * q;
-        phi_n = (p - 1) * (q - 1);
+        p = BigInteger.probablePrime(bitLength, secureRandom);
+        q = BigInteger.probablePrime(bitLength, secureRandom);
+        n = p.multiply(q);
+        phi_n = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
 
-        long e = calculate_e();
-
-        if (e == 0) {
-            System.out.println("Value out of range");
-            System.exit(0);
+        e = BigInteger.probablePrime(bitLength / 2, secureRandom);
+        while (e.gcd(phi_n).compareTo(BigInteger.ONE) != 0 && e.compareTo(phi_n) < 0) {
+            e = e.add(BigInteger.ONE);
         }
 
-        long d = calculate_d(e);
+        d = e.modInverse(phi_n);
 
-        long m, c;
-        while (true) {
-            System.out.println("\n1. Encrypt\n2. Decrypt\n3. Exit");
-            int choice = scanner.nextInt();
-            switch (choice) {
-            case 1:
-                System.out.println("Enter Message");
-                m = scanner.nextLong();
-                c = encrypt(m, e);
-                System.out.println("Encrypted Message: " + c);
-                break;
-            case 2:
-                System.out.println("Enter Cipher");
-                c = scanner.nextLong();
-                m = decrypt(c, d);
-                System.out.println("Decrypted Message: " + m);
-                break;
-            case 3:
-                System.exit(0);
-            }
-        }
+        System.out.println("P assigned as: " + p);
+        System.out.println("Q assigned as: " + q);
+        System.out.println("N assigned as: " + n);
+        System.out.println("PHI_N assigned as: " + phi_n);
+
+        System.out.println("\nEnter Message");
+        String msg = scanner.next();
+
+        String encryptedMessage = encrypt(msg);
+        System.out.println("Encrypted Message: " + encryptedMessage);
+
+        String decryptedMessage = decrypt(encryptedMessage);
+        System.out.println("Decrypted Message: " + decryptedMessage);
+
     }
 }
 
-/////////
-// Output
-// Enter two prime numbers
-// 3 11
-// 1. Encrypt
-// 2. Decrypt
-// 3. Exit
-// 1
-// Enter Message
-// 9
-// Encrypted Message: 3
-// 1. Encrypt
-// 2. Decrypt
-// 3. Exit
-// 2
-// Enter Cipher
-// 3
-// Decrypted Message: 9
-// 1. Encrypt
-// 2. Decrypt
-// 3. Exit
-// 3
+////////////
+//    Output
+//    P assigned as: 635469119130037
+//    Q assigned as: 954033057154061
+//    N assigned as: 606258546450627387595401630257
+//    PHI_N assigned as: 606258546450625798093225346160
+//
+//    Enter Message
+//    45465
+//    Encrypted Message: 581551797190538649820014908031
+//    Decrypted Message: 45465
